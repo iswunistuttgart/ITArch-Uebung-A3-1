5f2901b8e446b42849d6f5bf2f69e2228e6c2098
Matthias Strljic <nalim2@users.noreply.github.com>
2019-01-23 15:59:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,157 +3,41 @@ import logo from './ISW-Logo.jpg';
 import './App.css';
 import QRCode from 'qrcode.react';
 import 'whatwg-fetch';
-import 'kafka-rest';
-//import amqp from 'amqplib/callback_api';
-//
-//import callback_api from 'amqplib';
-//import amqp from "./index";
+
+var rest = require('rest');
+var ServerIP = 'http://193.196.37.135:8080';
 
 
-var KafkaRest = require('kafka-rest');
-var kafka = new KafkaRest({ 'url': 'http://193.196.37.135:8082' });
+
 var THECOMPONENT;
-var CurrentConsumer, CurrentConsumerbest;
 var queueTopic = 'QRCode-Read';
-var bestQRTopic = '';
-var teamTopicPreFix = 'TEAM-';
-var bestTeamTopicPreFix = 'BEST-CODE-CHANNEL-TEAM-';
-var consumerCreated = true;
 function subTopic() {
-    if(consumerCreated){
-        console.log("Initialisiere Team-Subscriber");
-        kafka.consumer("console-consumer-" + Math.round(Math.random() * 10000000000)).join({
-            "format": "binary",
-            "auto.commit.enable": "false",
-            'auto.offset.reset' : 'smallest'
-        }, function(err, consumer_instance) {
-            CurrentConsumer =consumer_instance;
-            //CurrentConsumerbest = consumer_instance;
-            if (err) return console.log("Failed to create instance in consumer group: " + err);
-            subTeam();
-
-        });
-
-
-        // Consumer for best Messsage
-
-        console.log("Initialisiere BestQR-Subscriber");
-        kafka.consumer("console-consumer-" + Math.round(Math.random() * 10000000000)).join({
-            "format": "binary",
-            "auto.commit.enable": "false",
-            'auto.offset.reset': 'smallest'
-        }, function (err, consumer_instance) {
-            CurrentConsumerbest = consumer_instance;
-            if (err) return console.log("Failed to create instance in consumer group: " + err);
-            subBest();
-        });
-
-        consumerCreated = false;
-    }else{
-        subTeam();
-        subBest();
-    }
-
-
-
-
-
-}
-function subTeam(){
-
-    console.log("Consumer TEAM  instance initialized: " + CurrentConsumer.toString() + " TOPIC: " + queueTopic);
-    var stream = CurrentConsumer.subscribe(queueTopic);
-    stream.on('data', function(msgs) {
-        console.log("YEAAA message TEAM : " + queueTopic);
-        for(var i = 0; i < msgs.length; i++){
-            //console.log(msgs[i].value.toString('utf8'));
-            var jsonObject = JSON.parse(msgs[i].value);
-            if (THECOMPONENT !== undefined) THECOMPONENT.setState({
-                value: "" + jsonObject.QRCode,
-                speed: jsonObject.Speed
-            })
+    rest(ServerIP + '/getBestCode/').then(function (response) {
+        console.log('response: ', response.entity);
+        var jsonObject = JSON.parse(response.entity.toString());
+        if (THECOMPONENT !== undefined) {
+            console.log('response: ', jsonObject);
+            THECOMPONENT.setState({
+                bestcode: "" + jsonObject.qrcode,
+                bestTeam: jsonObject.team,
+                bestSpeed: jsonObject.speed
+            });
         }
+    });
+    rest(ServerIP + '/getQRCode/' + THECOMPONENT.state.InputFieldvalue).then(function (response) {
+        console.log('response: ', response.entity);
+        var jsonObject = JSON.parse(response.entity.toString());
+        console.log('response: ', jsonObject.qrcode);
 
-
+        if (THECOMPONENT !== undefined) THECOMPONENT.setState({
+            value: "" + jsonObject.qrcode,
+            speed: jsonObject.speed
+        })
+    });
+    new Promise(function () {
+        setTimeout(subTopic, 2000);
     });
 
-    stream.on('error', function(err) {
-        console.log("Consumer TEAM instance reported an error: " + err);
-        console.log("Attempting TEAM to shut down consumer instance...");
-        setTimeout(function() {
-            // rest of code here
-            //subTeam();
-        }, 1000);
-
-    });
-    stream.on('end', function() {
-        console.log("Consumer TEAM stream closed.");
-        setTimeout(function() {
-            // rest of code here
-            subTeam();
-        }, 5000);
-    });
-
-    // Events are also emitted by the parent consumer_instance, so you can either consume individual streams separately
-    // or multiple streams with one callback. Here we'll just demonstrate the 'end' event.
-    CurrentConsumer.on('end', function() {
-        console.log("Consumer instance closed.");
-    });
-}
-
-function  subBest(){
-
-    console.log("Consumer BEST instance initialized: " + CurrentConsumerbest.toString() + " TOPIC: " + bestQRTopic);
-    var stream = CurrentConsumerbest.subscribe(bestQRTopic);
-    stream.on('data', function (msgs) {
-        console.log("YEAAA BEST message");
-        for (var i = 0; i < msgs.length; i++) {
-            //console.log(msgs[i].value.toString('utf8'));
-            var jsonObject = JSON.parse(msgs[i].value);
-            if (THECOMPONENT !== undefined) THECOMPONENT.setState({
-                bestcode: "" + jsonObject.QRCode,
-                bestTeam: jsonObject.Team,
-                bestSpeed: jsonObject.Speed
-            })
-        }
-
-
-    });
-
-    stream.on('error', function (err) {
-        console.log("Consumer BEST instance reported an error: " + err);
-        console.log("Attempting BEST to shut down consumer instance...");
-        CurrentConsumerbest.shutdown(function logShutdown(err) {
-                if (err)
-                    console.log("Error BEST while shutting down: " + err);
-                else
-                    console.log("Shutdown BEST cleanly.");
-
-            }
-
-        );
-        setTimeout(function() {
-            // rest of code here
-            //subBest();
-        }, 5000);
-
-
-        //new Promise(subTopic);
-
-    });
-    stream.on('end', function () {
-        console.log("Consumer BEST stream closed.");
-        setTimeout(function() {
-            // rest of code here
-            subBest();
-        }, 5000);
-    });
-
-    // Events are also emitted by the parent consumer_instance, so you can either consume individual streams separately
-    // or multiple streams with one callback. Here we'll just demonstrate the 'end' event.
-    CurrentConsumerbest.on('end', function () {
-        console.log("Consumer instance closed.");
-    });
 }
 class App extends Component {
     code = "";
@@ -162,7 +46,6 @@ class App extends Component {
         super(props);
         THECOMPONENT = this;
         console.log("Initialisiere die Homepage");
-        subTopic();
         this.state = {
             value: this.code,
             InputFieldvalue: queueTopic,
@@ -171,6 +54,7 @@ class App extends Component {
             bestTeam: '',
             bestSpeed: 0
         };
+        subTopic();
     }
     render() {
         return (
@@ -185,21 +69,21 @@ class App extends Component {
                 </p>
 
 
-                <p className="App-intro" >
+                <p className="App-intro">
                     Der letzte erhaltene QRCode: {this.state.value}
                 </p>
-                <p className="App-QRField" >
+                <p className="App-QRField">
                     Mit der aktuellen Geschwindigkeit von: {this.state.speed * 100}%
                 </p>
-                <p className="App-QRField" >
+                <p className="App-QRField">
                     <QRCode value={this.state.value} size={1.5 * 256}/>
                 </p>
 
-                <p className="App-QRField" >
+                <p className="App-QRField">
                     Aktuell bester empfangener QRCode kommt von Team: {this.state.bestTeam} mit der Geschwindigkeit
                     von: {this.state.bestSpeed * 100}%
                 </p>
-                <p className="App-QRField" >
+                <p className="App-QRField">
                     <QRCode value={this.state.bestcode} size={0.5 * 256}/>
                 </p>
 
@@ -210,13 +94,6 @@ class App extends Component {
     }
     updateInputValue(evt){
         THECOMPONENT.setState({InputFieldvalue:evt.target.value});
-        queueTopic = teamTopicPreFix + evt.target.value;
-        bestQRTopic = bestTeamTopicPreFix + evt.target.value;
-        THECOMPONENT.shutdownSubsAndResub();
-    }
-
-    shutdownSubsAndResub(){
-        subTopic();
     }
 
     getValue() {
