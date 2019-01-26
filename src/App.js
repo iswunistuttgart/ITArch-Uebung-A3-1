19471b12c6a0983bec418a704c753aa2866660a4
derEskimo <hannes.zakes@gmx.de>
2019-01-26 18:10:44 +0100

    Use particelsJS as Background

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import logo from './ISW-Logo.jpg';
 import './App.css';
 import QRCode from 'qrcode.react';
 import 'whatwg-fetch';
+import particlesJS from 'particles.js'
 
 var rest = require('rest');
 var ServerIP = 'http://193.196.37.135:8080';
 
 
-
 var THECOMPONENT;
 var queueTopic = 'QRCode-Read';
+
 function subTopic() {
 
     rest(ServerIP + '/getBestCode/').then(function (response) {
@@ -31,13 +32,13 @@ function subTopic() {
         var jsonObject = JSON.parse(response.entity.toString());
         console.log('response: ', jsonObject.qrcode);
 
-        if (THECOMPONENT !== undefined)  THECOMPONENT.setState({
-                value: "" + jsonObject.qrcode,
-                speed: jsonObject.speed
-            });
+        if (THECOMPONENT !== undefined) THECOMPONENT.setState({
+            value: "" + jsonObject.qrcode,
+            speed: jsonObject.speed
+        });
 
         //prüfe und kennzeichne ob Team existiert
-        if (jsonObject.team === "Kein TEAM" && jsonObject.qrcode === "Kein CODE"){
+        if (jsonObject.team === "Kein TEAM" && jsonObject.qrcode === "Kein CODE") {
             console.log('Team not found');
             document.getElementById("TeamInput").className = 'App-input warning';
         } else {
@@ -51,6 +52,8 @@ function subTopic() {
 
 
 }
+
+
 class App extends Component {
     code = "";
 
@@ -67,57 +70,66 @@ class App extends Component {
             bestSpeed: 0
         };
         subTopic();
+
+        /* particlesJS.load(@dom-id, @path-json, @callback (optional));*/
+        const particles = window.particlesJS;
+        particles.load('particles-js', 'particles/particlesjs-isw3.json', function () {
+            console.log('callback - particles.js config loaded');
+        });
     }
+
     render() {
         return (
             <div className="App">
+                <div id="particles-js"></div>
+
+
                 <header className="App-header">
                     <a href="https://www.isw.uni-stuttgart.de/" target="_blank">
                         <img src={logo} className="App-logo" alt="logo"/>
                     </a>
                     <h1 className="App-title">Welcome to QRCode display service</h1>
                 </header>
-                <p>
-                    Gib hier deinen Team-Namen ein: <input id="TeamInput" className="App-input normal" value={this.state.InputFieldvalue}
-                                                           onChange={this.updateInputValue}/>
-                </p>
 
 
-                <p className="App-intro">
-                    Der letzte erhaltene QRCode:
-                </p>
-                <p className="App-QRField">
-                    Mit der aktuellen Geschwindigkeit von: {this.state.speed * 100}%
-                </p>
+                <div className="App-flex">
+                    <p className="App-intro">
+                        Gib hier deinen Team-Namen ein: <input id="TeamInput" className="App-input normal"
+                                                               value={this.state.InputFieldvalue}
+                                                               onChange={this.updateInputValue}/>
+                    </p>
 
-                <div className="flip-container">
-                <div className="flipper">
-                    <div className="front">
-                        <QRCode value={this.state.value} size={1.5 * 256}/>
+
+                    <p className="App-intro">Der letzte erhaltene QRCode:</p>
+                    <p>Mit der aktuellen Geschwindigkeit von: {this.state.speed * 100}%</p>
+
+
+                    <div className="flip-container">
+                        <div className="flipper">
+                            <div className="front">
+                                <QRCode value={this.state.value} size={1.5 * 256}/>
+                            </div>
+                            <div className="back">
+                                <p>Im QR-Code enthaltene Nachricht:</p>
+                                <p><b>{this.state.value}</b></p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="back">
-                        <p>Im QR-Code enthaltene Nachricht:</p>
-                        <p><b>{this.state.value}</b></p>
+
+
+                    <p>Aktuell bester empfangener QRCode kommt von Team:&nbsp;<b> {this.state.bestTeam} </b> mit der
+                        Geschwindigkeit von: {this.state.bestSpeed * 100}%</p>
+                    <div className="App-flex itemQR">
+                        <QRCode value={this.state.bestcode} size={0.5 * 256}/>
                     </div>
                 </div>
-                </div>
-
-
-                <p className="App-QRField">
-                    Aktuell bester empfangener QRCode kommt von Team:&nbsp;<b> {this.state.bestTeam} </b> &nbsp;mit der Geschwindigkeit
-                    von: {this.state.bestSpeed * 100}%
-                </p>
-                <p className="App-QRField">
-                    <QRCode value={this.state.bestcode} size={0.5 * 256}/>
-                </p>
-
             </div>
+
         );
-
-
     }
-    updateInputValue(evt){
-        THECOMPONENT.setState({InputFieldvalue:evt.target.value});
+
+    updateInputValue(evt) {
+        THECOMPONENT.setState({InputFieldvalue: evt.target.value});
     }
 
     getValue() {
